aa07dd2ce87b81c4846013e796fad59d25ef4599
Sean <sean@senseirat.com>
2022-03-21 20:24:12 -0500

    Adjusting name, editing note about fields

--- back-end/dynamodb.tf
+++ back-end/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "resume-visit-counter" {
-  name         = "Cloud-Resume-Visitors-test"
+  name         = "Cloud-Resume-Visitors"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ip_address"
   table_class  = "STANDARD_INFREQUENT_ACCESS"
@@ -12,4 +12,4 @@ resource "aws_dynamodb_table" "resume-visit-counter" {
   tags = var.common_tags
 }
 
-# Attributes are ip_address, visit_count, and country
+# Attributes are ip_address, visit_count, log_source, and country
