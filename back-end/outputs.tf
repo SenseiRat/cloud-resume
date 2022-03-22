a0be1f42557820f09b174586a933f2d4dd252921
Sean <sean@senseirat.com>
2022-03-21 20:23:08 -0500

    Adding outputs to do some dynamic configuration

---
+++ back-end/outputs.tf
@@ -0,0 +1,17 @@
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.resume-visit-counter.name
+}
+
+output "resume_bucket_name" {
+  value = aws_s3_bucket.resume-bucket.id
+}
+
+output "cf_dist_id" {
+  value = aws_cloudfront_distribution.resume-distribution.id
+}
+
+data "aws_region" "current" {}
+
+output "aws_region" {
+  value = data.aws_region.current.name
+}
