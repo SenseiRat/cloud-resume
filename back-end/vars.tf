adb19f7f570adb42ec6ce5e908481bc485d57115
Sean <sean@senseirat.com>
2022-03-23 17:53:46 -0500

    Changing way env vars are handled

--- back-end/vars.tf
+++ back-end/vars.tf
@@ -8,53 +8,46 @@ variable "common_tags" {
 }
 
 variable "aws_access_key" {
-  type    = string
-  default = "$env:AWS_ACCESS_KEY_ID"
+  type      = string
+  sensitive = true
 }
 
 variable "aws_secret_key" {
-  type    = string
-  default = "$env:AWS_SECRET_ACCESS_KEY"
+  type      = string
+  sensitive = true
 }
 
 variable "aws_region" {
   type        = string
-  default     = "$env:AWS_REGION"
   description = "Region for region-specific resources"
 }
 
 variable "backend_bucket" {
   type        = string
   description = "The name of the bucket used for the backend"
-  default     = "$env:BACKEND_BUCKET"
 }
 
 variable "resume_bucket" {
   type        = string
   description = "The S3 bucket used to host the website"
-  default     = "$env:RESUME_BUCKET"
 }
 
 variable "cloudfront_distribution_id" {
   type        = string
   description = "The Cloudfront distribution ID"
-  default     = "$env:CF_DIST_ID"
 }
 
 variable "dynamodb_table_name" {
   type        = string
   description = "The name of the DynamoDB Table that the lambda interacts with"
-  default     = "$env:DYNAMODB_TABLE_NAME"
 }
 
 variable "domain_name" {
   type        = string
   description = "The domain name for the website"
-  default     = "$env:DOMAIN_NAME"
 }
 
 variable "cicd-resume-policy" {
   type        = string
   description = "Stored because we can't call an object within itself"
-  default     = "$env:CICD_RESUME_POLICY"
 }
