9fcd3e5df74f0f39ba4d660bcb7b5417aad500e6
Sean <sean@senseirat.com>
2022-03-23 16:13:59 -0500

    Disabling github secrets

--- back-end/github_secrets.tf
+++ back-end/github_secrets.tf
@@ -1,5 +1,5 @@
-resource "github_actions_secret" "dynamodb_table_name" {
-  repository      = "cloud-resume"
-  secret_name     = "DYNAMODB_TABLE_NAME"
-  plaintext_value = aws_dynamodb_table.resume-visit-counter.name
-}
+#resource "github_actions_secret" "dynamodb_table_name" {
+#  repository      = "cloud-resume"
+#  secret_name     = "DYNAMODB_TABLE_NAME"
+#  plaintext_value = aws_dynamodb_table.resume-visit-counter.name
+#}
