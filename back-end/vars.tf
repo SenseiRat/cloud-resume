f4f51aa8446fb470f5a2a4d64f11d336aa776f26
Sean <sean@senseirat.com>
2022-03-23 14:26:09 -0500

    Updating variables to be pulled from environment to prep for dynamically updating

--- back-end/vars.tf
+++ back-end/vars.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+variable "common_tags" {
+  description = "Common tags applied to all components"
+}
+
 variable "aws_access_key" {
   type    = string
   default = "$env:AWS_ACCESS_KEY_ID"
@@ -13,22 +19,38 @@ variable "aws_region" {
   description = "Region for region-specific resources"
 }
 
+variable "backend_bucket" {
+  type = string
+  description = "The name of the bucket used for the backend"
+  default = "$env:BACKEND_BUCKET"
+}
+
+variable "resume_bucket" {
+  type = string
+  description = "The S3 bucket used to host the website"
+  default = "$env:RESUME_BUCKET"
+}
+
+variable "cloudfront_distribution_id" {
+  type = string
+  description = "The Cloudfront distribution ID"
+  default = "$env:CF_DIST_ID"
+}
+
+variable "dynamodb_table_name" {
+  type = string
+  description = "The name of the DynamoDB Table that the lambda interacts with"
+  default = "$env:DYNAMODB_TABLE_NAME"
+}
+
 variable "domain_name" {
   type        = string
   description = "The domain name for the website"
+  default = "$env:DOMAIN_NAME"
 }
-
-variable "common_tags" {
-  description = "Common tags applied to all components"
-}
-
-variable "backend_bucket" {
-  description = "The name of the bucket used for the backend"
-}
-
-data "aws_caller_identity" "current" {}
 
 variable "cicd-resume-policy" {
   type        = string
   description = "Stored because we can't call an object within itself"
+  default = "$env:CICD_RESUME_POLICY"
 }
